d0eeadaec65d4ac7ce589bb821dbc1b8b40ea775
Philip Bauer <bauer@starzel.de>
2012-11-10 16:13:35 +0100

    only hide after success

--- src/collective/noticeboard/resources/noticeboard.js
+++ src/collective/noticeboard/resources/noticeboard.js
@@ -198,9 +198,11 @@
                     });
                     this.$el.find(".publish a").click(function(event) {
                         event.preventDefault();
-                        $.post(this.href);
-                        $(this).closest(".note").find("span.review-state").remove();
-                        $(this).parent().remove();
+                        var link = $(this);
+                        $.post(this.href, function() {
+                            link.closest(".note").find("span.review-state").remove();
+                            link.parent().remove();
+                        });
                     });
                     this.$el.bind("click.zindex", this.updateZIndex);
                     this.$el.bind("click.edit", this.updateEditBar);
