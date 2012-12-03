79de0b97c3f12731582ee99a54f4c84d5409b5f3
Philip Bauer <bauer@starzel.de>
2012-12-03 16:45:45 +0100

    only publish if folder is actually a noticeboard

--- src/collective/noticeboard/events.py
+++ src/collective/noticeboard/events.py
@@ -2,13 +2,13 @@ from Products.CMFCore.WorkflowCore import WorkflowException
 from collective.noticeboard.settings import NoticeboardSettings
 from collective.noticeboard.interfaces import INoticeboard
 from Products.CMFCore.utils import getToolByName
-from Acquisition import aq_parent
 import logging
 
 logger = logging.getLogger("collective.noticeboard")
 
+
 def auto_publish(note, event):
-    if INoticeboard.providedBy(event.newParent):
+    if INoticeboard.providedBy(event.newParent) and event.newParent.layout == 'noticeboardview':
         settings = NoticeboardSettings(event.newParent)
         if settings.publish_on_creation:
             workflowTool = getToolByName(note, "portal_workflow")
